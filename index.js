cfab7e4dec7a50e6d5ee28dafa5c48f20799c0ba
Neaz-mq <neazmorshed666@gmail.com>
2023-12-28 21:02:14 +0600

    cookie parser added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -9,9 +10,13 @@ const port = process.env.PORT || 5000;
 console.log(process.env.DB_PASS);
 
 // middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true
+}));
 
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zyo6s3.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -39,7 +44,16 @@ async function run() {
         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
           expiresIn: '1h'
       });
-            res.send( token )
+
+
+            res
+            .cookie('token', token, {
+              httpOnly: true,
+              secure: false
+              
+          })
+            
+            .send( { success: true } )
     })
 
 
@@ -70,6 +84,7 @@ async function run() {
 
    app.get('/bookings', async (req, res) => {
     console.log(req.query.email);
+    console.log('ttttt token', req.cookies.token)
     let query = {};
     if (req.query?.email) {
         query = { email: req.query.email }
